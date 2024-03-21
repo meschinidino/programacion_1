3de41b0e39327f406154c63a707f0e2704907a5a
meschinidino <francodmsoloing@gmail.com>
2024-03-21 10:24:56 -0300

    "actualizado hasta clase 2"

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from main import create_app
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+app = create_app()
 
-app = Flask(__name__)
+app.app_context().push()
 
 
 if __name__ == '__main__':
