051106c805f544697378036053b30b8a80c3a8cf
almaeq <alma.quinterosquiroga@gmail.com>
2024-03-28 18:52:03 -0300

    agregamos los recursos de libro y libros

--- backend/main/resources/books.py
+++ backend/main/resources/books.py
@@ -2,5 +2,48 @@ from flask_restful import Resource
 from flask import request
 
 BOOKS = {
-    1:{'name':'book1', 'author':'author1'}
+    1:{'name':'book1', 'author':'author1'},
+    2:{'name':'book2','author':'autho2'},
 }
+
+
+class Books(Resource):
+    #obtener lista de los libros
+    def get(self):
+        return BOOKS
+
+    #insertar recurso
+    def post(self):
+        animal = request.get_json()
+        id = int(max(BOOKS.keys()))+1
+        BOOKS[id] = animal
+        return BOOKS[id], 201
+    
+class Book(Resource):
+    #obtener recurso
+    def get(self, id):
+        #Verifico que exista el libro
+        if int(id) in Books:
+            #retorno libro
+            return Books[int(id)]
+        #Si no existe 404
+        return '', 404
+    
+    #Eliminar recurso
+    def delete(self, id):
+        #Verifico que exista el libro
+        if int(id) in Books:
+            #elimino libro
+            del Books[int(id)]
+            return '', 204
+        #Si no existe 404
+        return '', 404
+    
+    #Modificar el recurso libro
+    def put(self, id):
+        if int(id) in Books:
+            book = Books[int(id)]
+            data = request.get_json()
+            book.update(data)
+            return '', 201
+        return '', 404
