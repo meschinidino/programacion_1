3224f0ad48ea1c0d670326b6ecece7c5e1926779
jaidar2003 <j.aidar@alumno.um.edu.ar>
2024-05-01 20:53:25 -0300

    filtros

--- backend/main/resources/loans.py
+++ backend/main/resources/loans.py
@@ -6,8 +6,7 @@ from .. import db
 
 
 class Loans(Resource):
-
-    def get(self):
+    def get(sel):
         page = 1
 
         per_page = 10
@@ -19,6 +18,18 @@ class Loans(Resource):
         if request.args.get("per_page"):
             per_page = int(request.args.get("per_page"))
 
+
+
+        # if request.args.get("user_id"):
+        #     loans = loans.filter(LoansModel.user_id.like('%' + request.args.get("user_id") + '%'))
+        # if request.args.get("book_id"):
+        #     loans = loans.filter(LoansModel.book_id.like('%' + request.args.get("book_id") + '%'))
+        # if request.args.get("loan_date"):
+        #     loans = loans.filter(LoansModel.loan_date.like('%' + request.args.get("loan_date") + '%'))
+        # if request.args.get("sorting_by_finish_date"):
+        #     loans = loans.outerjoin(BooksModel).order_by(desc(BooksModel.finish_date))
+
+
         loans = loans.paginate(page=page, per_page=per_page, error_out=True)
 
         return jsonify({'loans': [loan.to_json() for loan in loans],
@@ -26,20 +37,21 @@ class Loans(Resource):
                         'pages': loans.pages,
                         'page': page})
 
+
+
+
     def post(self):
-        book_ids = request.json().get('book_id')
+        book_ids = request.get_json().get('book_id')
         loan = LoansModel.from_json(request.get_json())
 
         if book_ids:
 
             books = BooksModel.query.filter(BooksModel.loan_id.in_(book_ids)).all()
-
             loan.books.extend(books)
 
         db.session.add(loan)
         db.session.commit()
         return loan.to_json(), 201
-
 
 
 class Loan(Resource):
